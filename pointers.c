bdb2fa82fb7c6563104450e7d180f870b82c1780
Thomas Carey <scarey.tcareY@gmail.com>
2023-11-27 15:35:51 +0000

    added pointers.c

---
+++ pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int test(int *add) {
+    *add = 5;
+    printf("%p\n", add);
+    return 0;
+}
+
+int main() {
+    // Your code here
+    int num = 10;
+    printf("num is %d\n", num); // Returns 10
+    test(&num);
+    printf("num is %d\n", num); // Returns 5
+    return 0;
+}
