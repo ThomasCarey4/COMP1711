63482d7bc99068fd4594c38d2ced64151c33aff0
Thomas Carey <scarey.tcareY@gmail.com>
2023-11-27 16:03:09 +0000

    changed arr to array

--- pointer-arithmetic.c
+++ pointer-arithmetic.c
@@ -4,10 +4,10 @@ int test(int *add) {
     return 0;
 }
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    printf("%p\n", arr); // Returns memory address
-    test(arr); // Returns 1
-    test(arr + 1); // Returns 2
-    test(arr + 2); // Returns 3
+    int array[5] = {1, 2, 3, 4, 5};
+    printf("%p\n", array); // Returns memory address
+    test(array); // Returns 1
+    test(array + 1); // Returns 2
+    test(array + 2); // Returns 3
     return 0;
 }
